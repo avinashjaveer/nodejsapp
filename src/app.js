0e53aa68b3222827158e83bc805c6c3b32d33ec7
avinashjaveer <avinashjaveer@gmail.com>
2026-09-23 20:55:46 +0530

    Update database connection details in app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000; // You can change the port if needed
 // Establishing a connection with the database
 const connection = mysql.createConnection({
-  host: '52.23.224.218',
+  host: '172.31.29.132',
   user: 'root',
   password: 'Password@1234',
   database: 'sadhixdb'
